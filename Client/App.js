8407cacf42d19eb44d21e29d700552b58781de0b
Kyle Robinson <kyle709@gmail.com>
2018-03-20 11:10:02 -0400

    Rebase AWS branch

--- Client/App.js
+++ Client/App.js
@@ -1,10 +1,5 @@
 import boot from "./artifacts/boot/index";
-import Amplify from 'aws-amplify';
-import aws_exports from './src/aws-exports';
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-Amplify.configure(aws_exports)
 
 const app = boot();
 
-export default withAuthenticator(app);
+export default app;
